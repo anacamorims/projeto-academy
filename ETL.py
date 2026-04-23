4b3582ddd3c03445f2bfcea16eaf584957a579c7
AdrianaQMelo <adrianaqueirozdemelo@gmail.com>
2026-04-23 12:43:19 -0300

    Limpa ETL, evitando o máximo o hardcoding e adiciona one-hor encoding para tipo de quarto.

--- ETL.py
+++ ETL.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import sqlite3
 from sklearn.preprocessing import LabelEncoder
 
 # 1 EXTRAÇÃO
@@ -7,141 +6,19 @@ df_base_interna = pd.read_csv('bases/base_interna.csv')
 
 #Inicialização do LabelEncoder por coluna
 le_bairro= LabelEncoder();
-le_tipo_quarto= LabelEncoder();
+# le_tipo_quarto= LabelEncoder(); - não vou usar
 le_tipo_propriedade= LabelEncoder();
 
-# 2. TRANSFORMAÇÃO - SELEÇÃO DE COLUNAS para TRATAMENTO (Limpeza Bruta)
-colunas_tratamento = ["price","accommodates","bedrooms", "beds", "bathrooms_text", 
-"room_type", "review_scores_rating","minimum_nights", "number_of_reviews", "property_type",
-"latitude", "longitude", "neighbourhood_cleansed"]
-df_base_interna = df_base_interna[colunas_tratamento]
+# MAPA DE MAPEAMENTO - TIPO DE QUARTO
+mapa_tipo_quarto = {
+    "entire home/apt": "casa_apto_inteiro",
+    "hotel room": "quarto_hotel",
+    "private room": "quarto_privativo",
+    "shared room": "quarto_compartilhado"
+}
 
-df_base_interna ["ano"] = 2025
-df_base_interna ["mes"] = 9
-
-# TRATAMENTO DA COLUNA 'price'
-# RENOMEAR COLUNA 'price' PARA 'preco'
-df_base_interna.rename(
-    columns={'price': 'preco'},
-    inplace=True
-)
-df_base_interna ['preco'] = df_base_interna ['preco'].str.replace('$', '', regex=False).str.replace(',', '', regex=False)
-
-
-# TRATAMENTO DA COLUNA 'preco' PARA float
-df_base_interna ['preco'] = df_base_interna ['preco'].astype(float)
-
-
-# TRATAMENTO DA COLUNA 'accommodates'
-# RENOMEAR COLUNA 'accommodates' PARA 'hospedes'
-df_base_interna.rename(
-    columns={'accommodates': 'hospedes'},
-    inplace=True
-)
-
-
-#TRATAMENTO DA COLUNA 'bathrooms_text'
-# CRIAR COLUNA 'banheiros' A PARTIR DA COLUNA 'bathrooms_text'
-df_base_interna['banheiros'] = (
-    df_base_interna['bathrooms_text']
-    .str.extract(r'(\d+\.?\d*)')
-    .astype(float)
-)
-
-# REMOVER COLUNA 'bathrooms_text' APÓS EXTRAÇÃO DE INFORMAÇÃO
-df_base_interna.drop('bathrooms_text', axis=1, inplace=True)
-
-
-# TRATAMENTO DA CULUNA 'bedrooms'
-# RENOMEAR COLUNA 'bedrooms' PARA 'quartos'
-df_base_interna.rename(
-    columns={'bedrooms': 'quartos'},
-    inplace=True
-)
-
-# TRATAMENTO DA COLUNA 'beds'
-# RENOMEAR COLUNA 'beds' PARA 'camas'
-df_base_interna.rename(
-    columns={'beds': 'camas'},
-    inplace=True
-)
-
-# TRATAMENTO DA COLUNA 'room_type'
-# RENOMEAR COLUNA 'room_type' PARA 'tipo_de_quarto'
-df_base_interna.rename(
-    columns={'room_type': 'tipo_de_quarto'},
-    inplace=True
-)
-#Normalizar tipo_de_quarto - todos valores minúsculos
-df_base_interna['tipo_de_quarto'] = (
-    df_base_interna['tipo_de_quarto']
-    .str.strip()
-    .str.lower()
-)
-
-#LAbel encoder para 'tipo_de_quarto'
-#df_base_interna ["tipo_de_quarto_encode"] = le_tipo_quarto.fit_transform(df_base_interna ["tipo_de_quarto"]) + 1
-
-# # TRATAR VALORES NULOS - SUBSTITUIR POR 'moda'
-# df_base_interna['tipo_de_quarto'] = (
-#     df_base_interna['tipo_de_quarto'].fillna(df_base_interna['tipo_de_quarto'].mode()[0])
-# )
-
-# df_base_interna = pd.get_dummies(
-#     df_base_interna,
-#     columns=['tipo_de_quarto'],
-#     prefix='tipo_quarto',
-#     drop_first=False
-# )
-
-# TRATAMENTO DA COLUNA 'review_scores_rating'
-# RENOMEAR COLUNA 'review_scores_rating' PARA 'nota_avaliacao'
-df_base_interna.rename(
-    columns={'review_scores_rating': 'nota_avaliacao'},
-    inplace=True
-)
-
-# TRATAMENTO DA COLUNA 'minimum_nights'
-# RENOMEAR COLUNA 'minimum_nights' PARA 'noites_minimas'
-df_base_interna.rename(
-    columns={'minimum_nights': 'noites_minimas'},
-    inplace=True
-)
-
-# TRATAMENTO DA COLUNA 'number_of_reviews'
-# RENOMEAR PARA 'quantidade_avaliacoes'
-df_base_interna.rename(
-    columns={'number_of_reviews': 'quantidade_avaliacoes'},
-    inplace=True
-)
-
-# GARANTIR CONVERSÃO PARA NÚMERO
-df_base_interna['quantidade_avaliacoes'] = pd.to_numeric(
-    df_base_interna['quantidade_avaliacoes'],
-    errors='coerce'
-)
-
-# TRATAMENTO DA COLUNA 'property_type'
-# RENOMEAR COLUNA 'property_type' PARA 'tipo_de_propriedade'
-df_base_interna.rename(
-    columns={'property_type': 'tipo_de_propriedade'},
-    inplace=True
-)
-
-# DEFINIR A COLUNA DE PROPRIEDADE PARA TRATAMENTO
-coluna_propriedade = "tipo_de_propriedade"
-
-# NORMALIZAÇÃO
-df_base_interna[coluna_propriedade] = (
-    df_base_interna[coluna_propriedade]
-    .fillna("outros")
-    .astype(str)
-    .str.strip()
-    .str.lower()
-)
-
-# MAPA DE AGRUPAMENTO (REGRA DE NEGÓCIO)
-mapa_agrupamento = {
+# MAPA DE AGRUPAMENTO - TIPO DE PROPRIEDADE (REGRA DE NEGÓCIO)
+mapa_tipo_propriedade = {
 
     # ESPAÇO INTEIRO – RESIDENCIAL
     "entire home": "espaco_inteiro_residencial",
@@ -181,10 +58,107 @@ mapa_agrupamento = {
 
 }
 
+# 2. TRANSFORMAÇÃO - SELEÇÃO DE COLUNAS para TRATAMENTO (Limpeza Bruta)
+colunas_tratamento = ["price","accommodates","bedrooms", "beds", "bathrooms_text", 
+"room_type", "review_scores_rating","minimum_nights", "number_of_reviews", "property_type",
+"latitude", "longitude", "neighbourhood_cleansed"]
+df_base_interna = df_base_interna[colunas_tratamento]
+
+#inserir colunas de ano e mês para integrar ao ipca - pode mudar para base do AIRBNB ( adiconar coluna 'last_scraped' ou 'calendar_last_scraped' e extrair ano e mês a partir dela)
+df_base_interna ["ano"] = 2025
+df_base_interna ["mes"] = 9
+
+# RENOMEAR TODAS AS COLUNAS NO INÍCIO
+df_base_interna.rename(
+    columns={
+        'price': 'preco',
+        'accommodates': 'hospedes',
+        'bedrooms': 'quartos',
+        'beds': 'camas',
+        'room_type': 'tipo_de_quarto',
+        'review_scores_rating': 'nota_avaliacao',
+        'minimum_nights': 'noites_minimas',
+        'number_of_reviews': 'quantidade_avaliacoes',
+        'property_type': 'tipo_de_propriedade',
+        'neighbourhood_cleansed': 'bairro'
+    },
+    inplace=True
+)
+
+#atribui variáveis as colunas de interesse para tratamento - evitar hardcoding
+coluna_preco = "preco"
+coluna_hospedes = "hospedes"
+coluna_quartos = "quartos"
+coluna_camas = "camas"
+coluna_tipo_de_quarto = "tipo_de_quarto"
+coluna_nota_avaliacao = "nota_avaliacao"
+coluna_noites_minimas = "noites_minimas"
+coluna_quantidade_avaliacoes = "quantidade_avaliacoes"
+coluna_tipo_de_propriedade = "tipo_de_propriedade"
+coluna_bairro = "bairro"
+coluna_latitude = "latitude"
+coluna_longitude = "longitude"
+
+# TRATAMENTO DA COLUNA 'price'
+df_base_interna [coluna_preco] = df_base_interna [coluna_preco].str.replace('$', '', regex=False).str.replace(',', '', regex=False)
+# TRATAMENTO DA COLUNA 'preco' PARA float
+df_base_interna [coluna_preco] = df_base_interna [coluna_preco].astype(float)
+
+#TRATAMENTO DA COLUNA 'bathrooms_text'
+# CRIAR COLUNA 'banheiros' A PARTIR DA COLUNA 'bathrooms_text'
+df_base_interna['banheiros'] = (
+    df_base_interna['bathrooms_text']
+    .str.extract(r'(\d+\.?\d*)')
+    .astype(float)
+)
+# REMOVER COLUNA 'bathrooms_text' APÓS EXTRAÇÃO DE INFORMAÇÃO
+df_base_interna.drop('bathrooms_text', axis=1, inplace=True)
+
+#Normalizar tipo_de_quarto - todos valores minúsculos
+df_base_interna[coluna_tipo_de_quarto] = (
+    df_base_interna[coluna_tipo_de_quarto]
+    .str.strip()
+    .str.lower()
+)
+
+#Label encoder para 'tipo_de_quarto'
+#df_base_interna ["tipo_de_quarto_encode"] = le_tipo_quarto.fit_transform(df_base_interna ["tipo_de_quarto"]) + 1
+
+#Renomeor categorias de 'tipo de quarto' para português
+df_base_interna[coluna_tipo_de_quarto] = df_base_interna[coluna_tipo_de_quarto].map(mapa_tipo_quarto)
+
+df_base_interna_dummies = pd.get_dummies(
+    df_base_interna[coluna_tipo_de_quarto],
+    prefix='tipo_quarto',
+    drop_first=False
+)
+
+#juntar as colunas dummies com a base original
+df_base_interna = pd.concat([df_base_interna, df_base_interna_dummies])
+
+# TRATAMENTO DA COLUNA 'quantidade_avaliacoes'
+# GARANTIR CONVERSÃO PARA NÚMERO
+df_base_interna[coluna_quantidade_avaliacoes] = pd.to_numeric(
+    df_base_interna[coluna_quantidade_avaliacoes],
+    errors='coerce'
+)
+
+# DEFINIR A COLUNA DE PROPRIEDADE PARA TRATAMENTO
+#atribui uma variável para evitar hardcoding
+
+# NORMALIZAÇÃO
+df_base_interna[coluna_tipo_de_propriedade] = (
+    df_base_interna[coluna_tipo_de_propriedade]
+    .fillna("outros")
+    .astype(str)
+    .str.strip()
+    .str.lower()
+)
+
 # APLICAR AGRUPAMENTO
 df_base_interna["tipo_de_propriedade_grupo"] = (
-    df_base_interna[coluna_propriedade]
-    .map(mapa_agrupamento)
+    df_base_interna[coluna_tipo_de_propriedade]
+    .map(mapa_tipo_propriedade)
     .fillna("outros")
 )
 
@@ -193,17 +167,11 @@ df_base_interna ["tipo_de_propriedade_grupo"] = le_tipo_propriedade.fit_transfor
 
 # TRATAMENTO DA COLUNA 'latitude'
 # TRATAMENTO DA COLUNA 'latitude' PARA float
-df_base_interna ['latitude'] = df_base_interna ['latitude'].astype(float)
+df_base_interna [coluna_latitude] = df_base_interna [coluna_latitude].astype(float)
 
 # TRATAMENTO DA COLUNA 'longitude'
 # TRATAMENTO DA COLUNA 'longitude' PARA float
-df_base_interna ['longitude'] = df_base_interna ['longitude'].astype(float)
+df_base_interna [coluna_longitude] = df_base_interna [coluna_longitude].astype(float)
 
-# TRATAMENTO DA COLUNA "neighbourhood_cleansed"
-# RENOMEAR COLUNA 'neighbourhood_cleansed' PARA 'bairro'
-df_base_interna.rename(
-    columns={'neighbourhood_cleansed': 'bairro'},
-    inplace=True
-)
 # CRIAR VARIÁVEIS DO LABEL ENCODER 'bairro_grupo'
 df_base_interna ["bairro_encode"] = le_bairro.fit_transform(df_base_interna ["bairro"]) + 1
